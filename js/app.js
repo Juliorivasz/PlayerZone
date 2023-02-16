a8472158c5e924ca3e4e49362fd34ae03766500b
juliorivasz <julioandresrivas@gmail.com>
2023-02-16 16:03:05 -0300

    added one carousel

---
+++ js/app.js
@@ -0,0 +1,100 @@
+const big = document.querySelector('.big')
+const arrow = document.querySelectorAll('.arrow')
+const listArrow = document.querySelectorAll('.no-activo')
+const listArrowAqua = document.querySelectorAll('.activo')
+const point = document.querySelectorAll('.point')
+
+// cuando hago click en un punto
+    // Saber la posicion de ese punto
+    // Aplicar un transform traslateX al big
+    // Quitar la clase active de TODOS los puntos 
+    // Añadir la clase active al punto que hemos hecho CLICK 
+
+    // recorre el array de point
+point.forEach( (cadaPunto, i) => {
+    // al hacer click en un punto 
+    // cambia de imagen
+    point[i].addEventListener('click', ()=>{
+        let posicion = i;
+        let operacion = posicion * -50;
+
+        big.style.transform = `translateX(${ operacion }%)`;
+
+        point.forEach((cadaPunto, i)=> {
+            point[i].classList.remove('active');
+        })
+
+        point[i].classList.add('active');
+    });
+    // count es el contador y validador de la funcionalidad de cambiar
+    // la imagen en un intervalo de tiempo
+    let count = 0;
+    let flash = ()=>{
+        // cambia a la primera imagen
+        if(count) {
+            count = 0;
+            let posicion = count;
+            let operacion = posicion * -50;
+
+            // se le aplica un estilo a la etiqueta con la clase big 
+            big.style.transform = `translateX(${ operacion }%)`;
+
+            // se cambia las clases que muestra el punto seleccionado
+            point[1].classList.remove('active');
+            point[count].classList.add('active');
+        }else{
+            // cambia a la segunda imagen
+            count = 1;
+            let posicion = count;
+            let operacion = posicion * -50;
+
+            big.style.transform = `translateX(${ operacion }%)`;
+
+            point[0].classList.remove('active');
+            point[count].classList.add('active');
+        }
+
+    };
+    setInterval(flash,6000);
+})
+let posicion = 0;
+arrow.forEach((Flecha, i)=> {
+    Flecha.addEventListener('click', ()=> {
+        if(arrow[i].alt === 'arrow right'){
+            listArrow[1].className = 'activo right';
+            listArrowAqua[1].className = 'no-activo right';
+            desplazarImagen();          
+            setTimeout(()=>{
+                listArrow[1].className = 'no-activo right';
+                listArrowAqua[1].className = 'activo right';
+            },200)
+        }else {
+            listArrow[0].className = 'activo right';
+            listArrowAqua[0].className = 'no-activo right';
+            desplazarImagen();
+            setTimeout(()=>{
+                listArrow[0].className = 'no-activo right';
+                listArrowAqua[0].className = 'activo right';
+            },200)
+        }
+        
+    })
+})
+
+// funcion para desplazar la imagen al hacer click en las flechas
+const desplazarImagen = ()=>{
+    if(posicion){
+        posicion = 0;
+        var operacion = posicion * -50;
+        big.style.transform = `translateX(${ operacion }%)`;
+        point[1].classList.remove('active');
+        point[0].classList.add('active');
+    }else {
+        posicion = 1;
+        operacion = posicion * -50;
+        big.style.transform = `translateX(${ operacion }%)`;
+        point[0].classList.remove('active');
+        point[1].classList.add('active');             
+    }
+}
+ 
