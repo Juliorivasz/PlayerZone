912dd4be1692cfb4503cabc38da3a802fc5924be
juliorivasz <julioandresrivas@gmail.com>
2023-02-21 11:50:00 -0300

    modified styles and logic of card

--- js/card.js
+++ js/card.js
@@ -1,31 +1,32 @@
 // funcion para crear el card pasandole por parametro la informacion
 // que quiera tener en el card
 const products = document.querySelector('.products');
-const containerCard = document.querySelector('.container__card');
 
 function card(title, imagen, stock, price, financing) {
     const div = document.createElement('div');
     div.classList.add('container__card');
     div.innerHTML = `
-<div class="container__img__card">
-    <img class="img__card" src=${imagen} alt="i5 procesador">
-</div>
-<div class="container__info__card">
-    <h1 class="title__card">${title}</h1>
-    <div class="container__check__stock">
-        <img src=${stock ? "./img/check-circle-fill.svg" : "./img/x-circle-fill.svg"} alt="check__fill">
-        <span class="stock__products">Producto ${stock ? 'con' : 'sin'} Stock</span>
-    </div>
-    <span class="price__products">$ ${price} </span>
-    <p class="financing">
-        Hasta en 12 cuotas de $ ${Math.round(financing/12)}.000
-        Sin interés del precio de lista
-    </p>
-    <button class="btn__products" type="button">agregar</button>
-</div>`; 
+            <div class="container__img__card">
+                <img class="img__card" src=${imagen} alt="i5 procesador">
+            </div>
+            <div class="container__info__card">
+                <h1 class="title__card">${title}</h1>
+                <div class="container__check__stock">
+                    <img src=${stock ? "./img/check-circle-fill.svg" : "./img/x-circle-fill.svg"} alt="check__fill">
+                    <span class="stock__products">Producto ${stock ? 'con' : 'sin'} Stock</span>
+                </div>
+            <span class="price__products">$ ${price} </span>
+            <p class="financing">
+                Hasta en 12 cuotas de $ ${Math.round(financing/12)}.000
+                Sin interés del precio de lista
+            </p>
+            <button class="btn__products" type="button">agregar</button>
+            </div>`;
     products.appendChild(div);
 }
 card('i7 12600 Procesador', './img/Procesador-Intel-Core-i5-11400.png', false, '300.000', '400.000');
-card('i7 12600 Procesador', './img/Procesador-Intel-Core-i5-11400.png', true, '300.000', '400.000');
+card('i5 11400 Procesador', './img/Procesador-Intel-Core-i5-11400.png', true, '300.000', '400.000');
+card('i5 11400 Procesador', './img/Procesador-Intel-Core-i5-11400.png', true, '300.000', '400.000');
+card('i5 11400 Procesador', './img/Procesador-Intel-Core-i5-11400.png', true, '300.000', '400.000');
 export default card;
 
