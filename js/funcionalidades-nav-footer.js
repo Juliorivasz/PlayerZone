d4e9494f69d234e34698e609e709c621c0123fe9
rexdaro <rexdaro@gmail.com>
2023-02-26 11:20:14 -0300

    commit1

--- js/funcionalidades-nav-footer.js
+++ js/funcionalidades-nav-footer.js
@@ -1,16 +1,19 @@
 
- const button = document.querySelector('.logos-nav-lupa');
- const input = document.querySelector('.container-barra-busqueda');
-
+const button = document.querySelector('.logos-nav-lupa');
+const input = document.querySelector('.container-barra-busqueda');
+const div_barra_busqueda = document.querySelector('.div-barra-busqueda');
 
 
 
 button.addEventListener('click', () => {
     if(getComputedStyle(input).display === 'flex'){
-        input.style.display = 'none'
+        input.style.display = 'none' 
+        div_barra_busqueda.style = 'position: flex'  
+              
     }
     else{
-        input.style.display = 'flex'
+        input.style.display = 'flex'  
+        div_barra_busqueda.style = 'position: absolute; justify-content: center'
     }
 });
              
