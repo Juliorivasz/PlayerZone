6019d955172727cb7f27c03071c37e7c5a3993cf
rexdaro <rexdaro@gmail.com>
2023-02-26 20:31:24 -0300

    agregando funcionalidades al nav bar

--- js/funcionalidades-nav-footer.js
+++ js/funcionalidades-nav-footer.js
@@ -6,7 +6,7 @@ var imagenLupa = document.getElementById("logo_barra_busqueda");
 const inputBusqueda = document.querySelector("#id-barra-busqueda");
         
 
-
+//despliegue de barra de busqueda:
 
 button.addEventListener('click', () => {
     if(getComputedStyle(input).display === 'flex'){
@@ -25,11 +25,51 @@ button.addEventListener('click', () => {
         
     }
 });
-             
+        
 
+//despliegue del menu:
 
+const button_menu = document.querySelector('.btn-menu-responsive');
+const sub_menu = document.querySelector('.sub-menu');
+const cerrar_x_menu = document.querySelector('.logo_x');
 
+button_menu.addEventListener('click', () => {
+    if(getComputedStyle(sub_menu).display === 'block'){
+        sub_menu.style.display = 'none';         
 
+    }
+    else{
+        sub_menu.style.display = 'block'; 
+        casilla_usuario.style.display = 'none';        
+    }
+});
+
+cerrar_x_menu.addEventListener('click', () => {
+    sub_menu.style.display = 'none';
+});
+
+//despliegue del menu de usuario y carrito:
+
+const button_usuario = document.querySelector('.logo-user');
+const casilla_usuario = document.querySelector('.casilla-usuario');
+const cerrar_x_usuario = document.querySelector('.logo_x_usuario')
+
+button_usuario.addEventListener('click', () => {
+    if(getComputedStyle(casilla_usuario).display === 'flex'){
+        casilla_usuario.style.display = 'none'; 
+        
+        
+
+    }
+    else{
+        casilla_usuario.style.display = 'flex'; 
+        sub_menu.style.display = 'none';        
+    }
+});
+
+cerrar_x_usuario.addEventListener('click', () => {
+    casilla_usuario.style.display = 'none';
+});
 
 
  
