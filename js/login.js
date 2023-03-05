43484db327f28914e8663f8ffee791e05d8672e7
juliorivasz <julioandresrivas@gmail.com>
2023-03-05 00:28:35 -0300

    added view and hide password

--- js/login.js
+++ js/login.js
@@ -2,14 +2,45 @@ const formulario = document.querySelector('.form__login');
 
 formulario.addEventListener('submit', (event)=>{
     event.preventDefault();
-    const username = document.querySelector('#username').value.toLowerCase();
+    // usuario y contraseña
+    const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
+    const messageError = document.querySelectorAll('.message__error');
 
-    if(username === "admin" && password === "Admin123") {
+    // validacion de usuario y contraseña
+    if(username.toLowerCase() === "admin" && password === "Admin123") {
         window.location.href = "../index.html";
-    }else {
-        alert('usuario o contraseña incorrecta');
+    }else if(password !== "Admin123" && username === "admin"){
+        let valorCampo = username;
         event.target.reset();
+        messageError[0].classList.add('hidden');
+        messageError[1].classList.remove('hidden');
+        console.log(messageError);
+        document.querySelector('#username').value = valorCampo;
+    }else if(username !== "admin" && password === "Admin123"){
+        event.target.reset();
+        messageError[1].classList.add('hidden');
+        messageError[0].classList.remove('hidden');
+    }else {
+        event.target.reset();
+        messageError.forEach(e => e.classList = "message__error");
     }
+})
 
+const inputPassword = document.querySelector("#password");
+const eye = document.querySelectorAll(".eye");
+
+// mostrar u ocultar contraseña 
+eye.forEach((e) => {
+    e.addEventListener('click', ()=> {
+        if(eye[0].classList.value === "eye"){
+            eye[1].classList.remove('hidden');
+            eye[0].classList.add('hidden');
+            inputPassword.type = 'text';
+        }else if(eye[1].classList.value === "eye"){
+            eye[0].classList.remove('hidden');
+            e.classList.add('hidden');
+            inputPassword.type = 'password';
+        }
+})
 })
