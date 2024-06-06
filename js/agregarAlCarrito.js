290d2be15832de35cfe94c2a8c1582e26ec36b02
Julio Rivas <julioandresrivas@gmail.com>
2024-06-06 00:06:53 -0300

    Update agregarAlCarrito.js

--- js/agregarAlCarrito.js
+++ js/agregarAlCarrito.js
@@ -103,7 +103,7 @@ verPedido.addEventListener('click', ()=> {
     cerrarPopUp();
     pantallaDeCarga();
     setTimeout(()=> {
-      location.href= '/pages/carrito.html';
+      location.href= './pages/carrito';
     }, 1000)
 })
 
